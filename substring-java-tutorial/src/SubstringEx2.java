c7e3f8c4f4e0706f682baa6c48ec61734837bf5e
CodeLearnHub <93278003+CodeLearnHub@users.noreply.github.com>
2021-11-10 16:23:12 +0200

    Add files via upload
    Java substring tutorial example.

---
+++ substring-java-tutorial/src/SubstringEx2.java
@@ -0,0 +1,12 @@
+public class SubstringEx2 {
+    public static void main(String args[])
+    {
+        int beginning=12;
+        int ending=23;
+        String Str = new String("CodeLearnHub substring Tutorial");
+
+
+        System.out.print("The substring  is : ");
+        System.out.println(Str.substring(beginning,ending));
+    }
+}
