d8e60f0c923b949b0736abf96b3c4baffa2a0b7d
CodeLearnHub <93278003+CodeLearnHub@users.noreply.github.com>
2021-11-10 16:29:09 +0200

    Java substring tutorial example.

---
+++ substring-java-tutorial/src/SubstringEx1.java
@@ -0,0 +1,12 @@
+public class SubstringEx1 {
+        public static void main(String args[])
+        {
+            int beginning=23;
+            String Str = new String("CodeLearnHub substring Tutorial");
+
+
+            System.out.print("The substring  is : ");
+            System.out.println(Str.substring(beginning));
+        }
+    }
+
