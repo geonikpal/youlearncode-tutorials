99326b8194c000876f84878d6d60299db2e72a8c
geonikpal <geonikpal@gmail.com>
2021-11-20 18:40:19 +0200

    update .gitignore

--- math-random-tutorial/src/SimpleRandom.java
+++
@@ -1,8 +0,0 @@
-public class SimpleRandom {
-    public static void main(String[] args) {
-        //The function returns a random number between 0(inclusive) and 1(exclusive)
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Math.random());
-        }
-    }
-}
