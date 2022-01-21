4018cd3da0d71cf3f74dcad0b82ef567b67e6197
CodeLearnHub <93278003+CodeLearnHub@users.noreply.github.com>
2022-01-21 21:59:36 +0200

    Delete java-methods/src directory

--- java-methods/src/Overriding_example/Child.java
+++
@@ -1,7 +0,0 @@
-package Overriding_example;
-
-public class Child extends Parent{
-    public void method(){
-        System.out.println("This is the Child");
-    }
-}
--- java-methods/src/Overriding_example/Parent.java
+++
@@ -1,7 +0,0 @@
-package Overriding_example;
-
-public class Parent {
-    public void method(){
-        System.out.println("This is the Parent");
-    }
-}
