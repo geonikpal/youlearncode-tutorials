87340041572fc1972eaf5f40103e0d958bd5c098
CodeLearnHub <93278003+CodeLearnHub@users.noreply.github.com>
2022-01-28 21:17:26 +0200

    Delete Java Generics Tutorial/src/main/java directory

--- Java Generics Tutorial/src/main/java/Bounded_Types/BoundedTypeClassEx.java
+++
@@ -1,7 +0,0 @@
-package Bounded_Types;
-
-public class BoundedTypeClassEx<T extends Number>  {
-    public void method(int num) {
-        System.out.println(num);
-    }
-}
--- Java Generics Tutorial/src/main/java/Generic_Method/GenericMethodEx.java
+++
@@ -1,9 +0,0 @@
-package Generic_Method;
-
-public class GenericMethodEx {
-
-        public <T> void coinMethod(T coins) {
-            System.out.println("The are: " + coins+" coins");
-        }
-    }
-
